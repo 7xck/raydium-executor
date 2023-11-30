39aba4f688cab52b9ee9cdd56df96a616790032c
jackpashley <46322193+jackpashley@users.noreply.github.com>
2023-11-30 22:13:11 +1000

    add profit stats after trade

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 import traceback
 from typing import Any
+import time
 
 from loguru import logger
 
@@ -9,26 +10,33 @@ from CEX import CEX
 from raydium_amm import Liquidity
 from utils import purchase_info, sale_info
 
-logger.add("bot.csv", format="{time:YYYY-MM-DD HH:mm:ss},{level},{message}")
-
-amm: Liquidity
-cex: CEX
-config: Any
-
 # read json config file
 with open("config.json") as f:
     config = json.load(f)
 
 
 async def main():
-    global amm
+    size = 0.5
     amm = Liquidity(
         "https://api.mainnet-beta.solana.com",
         "5wyH6cE9qaNmxw9HyM2WVHZkQDXyrFVSgEMCDksbfjUP",
         config["private_key"],
-        "xd/usd",
+        "coin/sol",
     )
-    await amm.buy(0.5)
+    coin_balances = await amm.get_balance()
+    sol_before = coin_balances["sol"]
+
+    await amm.buy(size)
+    time.sleep(5)
+    coin_balances = await amm.get_balance()
+    sell_size = coin_balances["coin"]
+    await amm.sell(sell_size)
+    time.sleep(5)
+    coin_balances = await amm.get_balance()
+    sol_after = coin_balances["sol"]
+    print({"before": sol_before}, {"after": sol_after})
+    print({"profit": sol_after - sol_before})
+    print("return, ", sol_after / sol_before - 1)
 
 
 if __name__ == "__main__":
